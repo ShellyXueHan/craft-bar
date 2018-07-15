fa7cb3f3454b3d5920da8211714aa1c4b882577a
ShellyXueHan <shelly@freshworks.io>
2018-07-14 18:59:24 -0700

    formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BeerDetailsComponent } from './beers/beer-details/beer-details.component';
+import { BeerListComponent } from './beers/beer-list/beer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerDetailsComponent,
+    BeerListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
