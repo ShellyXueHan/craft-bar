08aabb9a251d33edef892a6e67130dc087a81ab4
ShellyXueHan <shelly@freshworks.io>
2018-07-17 14:15:34 -0700

    clean up scripts

--- routes/beer.js
+++ routes/beer.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// Get the beer functions:
+const validateBeer = require('../scripts/beerCheck').validateBeer;
+const isValidID = require('../scripts/beerCheck').isValidID;
+const renderBeer = require('../scripts/beerCheck').renderBeer;
+const isValidBeer = require('../scripts/beerCheck').isValidBeer;
+
 // Connection to db:
 const knex = require('../db/knex');
 
@@ -94,79 +100,5 @@ router.delete('/:id', (req, res) => {
     });
   }
 });
-
-
-/**
- * This function validates a beer and provide response:
- *  - if beer is valid:
- *    - get the fields from request body (coorespond to the names in views)
- *    - callback
- *  - if not:
- *    - 500 error
-**/
-function validateBeer(req, res, callback) {
-  console.log('validate beer');
-  if(isValidBeer(req.body)) {
-    const beer = {
-      name: req.body.name,
-      brewery: req.body.brewery,
-      type: req.body.type,
-      price: req.body.price
-    };
-    callback(beer);
-  } else {
-    res.status(500);
-    res.render('error', { message: 'Invalid id in 1!'});
-  }
-}
-
-/**
- * This function renders a beer and provide response:
- *  - if beer ID is valid:
- *    - get the beer details with the ID
- *    - render the beer
- *  - if not:
- *    - 500 error
-**/
-function renderBeer(id, res, viewName) {
-  console.log('render beer');
-  if(isValidID(id)) {
-    knex('beer')
-      .select()
-      .where('id', id)
-      .first()
-      .then(beer => {
-        res.render(viewName, beer);
-      });
-  } else {
-    res.status(500);
-    res.render('error', { message: 'Invalid id in 2!'});
-  }
-}
-
-/**
- * This function checks if the beer is valid:
- *  - not empty name
- *  - has a type as number
- *  - has non-empty brewery
- * 
-**/
-function isValidBeer(beer) {
-  console.log('check beer');
-  return typeof beer.name == 'string' &&
-          beer.name.trim() != '' &&
-          typeof beer.brewery == 'string' &&
-          beer.brewery.trim() != '' &&
-          typeof beer.type != 'undefined' &&
-          !isNaN(beer.type);
-}
-
-/**
- * This function checks if the beer is valid number:
-**/
-function isValidID(id) {
-  console.log('check id');
-  return !isNaN(id);
-}
 
 module.exports = router;
