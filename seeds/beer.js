381ff9c86a7d86b8364f7fefc154157f21ff24a0
ShellyXueHan <shelly@freshworks.io>
2018-07-17 23:56:44 -0700

    input more beers

--- seeds/beer.js
+++ seeds/beer.js
@@ -4,50 +4,66 @@ exports.seed = function(knex, Promise) {
   return knex('beer').del()
     .then(function () {
       const beers = [{
-        name: 'beer1',
-        brewery: 'red house',
+        name: '1792 english ale',
+        brewery: 'Sons Of Kent',
         type: 1,
         price: 15,
       }, {
-        name: 'beer2',
-        brewery: 'blue house',
-        type: 2,
+        name: 'camerons cosmic cream ale',
+        brewery: 'Camerons Brewing Company',
+        type: 1,
         price: 23,
       }, {
-        name: 'beer3',
-        brewery: 'yellow house',
+        name: 'flying monkeys amber ale',
+        brewery: 'Flying Monkeys Craft Brewery',
+        type: 1,
+        price: 25,
+      }, {
+        name: 'budweiser',
+        brewery: 'Labatt',
+        type: 2,
+        price: 13,
+      }, {
+        name: 'carlsberg',
+        brewery: 'Carlsberg International A/S',
+        type: 2,
+        price: 33,
+      }, {
+        name: 'corona extra',
+        brewery: 'Cerveceria Modelo S.A. De C.V.',
+        type: 2,
+        price: 25,
+      }, {
+        name: 'belhaven black scot stout',
+        brewery: 'Greene King Plc',
         type: 3,
+        price: 12,
+      }, {
+        name: 'guinness pub draught',
+        brewery: 'Diageo',
+        type: 3,
+        price: 15,
+      }, {
+        name: 'wellington imperial stout',
+        brewery: 'Wellington County',
+        type: 3,
+        price: 23,
+      }, {
+        name: 'poppers pink',
+        brewery: 'Brew Alliance',
+        type: 4,
+        price: 22,
+      }, {
+        name: 'mad jack',
+        brewery: 'Molson',
+        type: 4,
+        price: 18,
+      }, {
+        name: 'rickards radler',
+        brewery: 'Molson',
+        type: 4,
         price: 15,
       }];
       return knex('beer').insert(beers);
     });
 };
-
-// const beers = [{
-//   name: 'beer1',
-//   brewery: 'red house',
-//   type: 1,
-//   price: 15,
-//   description: 'best beer',
-//   rank: 1,
-//   alcpercent: 10,
-//   color: 1
-// }, {
-//   name: 'beer2',
-//   brewery: 'blue house',
-//   type: 2,
-//   price: 23,
-//   description: 'best beer',
-//   rank: 2,
-//   alcpercent: 10,
-//   color: 1
-// }, {
-//   name: 'beer3',
-//   brewery: 'yellow house',
-//   type: 3,
-//   price: 15,
-//   description: 'best beer',
-//   rank: 3,
-//   alcpercent: 10,
-//   color: 1
-// }];
